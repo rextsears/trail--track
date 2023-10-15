68d95a834665adccf987fbd9403fd610a536ec03
rextsears <srtm88@gmail.com>
2023-10-15 13:34:16 -0400

    map functionality

--- trail--track/trail-track/src/components/ActivityDetail.js
+++ trail--track/trail-track/src/components/ActivityDetail.js
@@ -1,55 +1,59 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getActivities } from '../api/trackServer.js'; // Import the getActivities function
+import { getActivities } from '../api/trackServer.js';
+import TrackMap from './TrackMap'; // Import the TrackMap component
+import '../styles/map.css'; // Import the map.css file
 
 function ActivityDetail() {
     const { id } = useParams();
-    const [activity, setActivity] = useState(null); // Use null as the initial state
+    const [activity, setActivity] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-  
+
     useEffect(() => {
-      // Fetch the specific activity by ID
-      getActivities()
-        .then((response) => {
-          // Find the activity with the matching ID
-          const matchedActivity = response.find((activity) => activity._id === id);
+        getActivities()
+            .then((response) => {
+                const matchedActivity = response.find((activity) => activity._id === id);
 
-          if (matchedActivity) {
-            setActivity(matchedActivity);
-            setLoading(false);
-            setError(null);
-          } else {
-            setError('Activity not found');
-            setLoading(false);
-          }
-        })
-        .catch((error) => {
-          console.error('Error fetching activities:', error);
-          setError('Error fetching activities');
-          setLoading(false);
-        });
-    }, [id]); // Re-run the effect when the 'id' parameter changes
+                if (matchedActivity) {
+                    setActivity(matchedActivity);
+                    setLoading(false);
+                    setError(null);
+                } else {
+                    setError('Activity not found');
+                    setLoading(false);
+                }
+            })
+            .catch((error) => {
+                console.error('Error fetching activities:', error);
+                setError('Error fetching activities');
+                setLoading(false);
+            });
+    }, [id]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
 
-  // Render the activity details
-  return (
-    <div>
-      <h2>Activity Detail</h2>
-      <p>Activity Type: {activity.activityType}</p>
-      <p>Location: {activity.location}</p>
-      <p>Completion Time: {activity.completionTime}</p>
-      <p>Distance: {activity.distance}</p>
-      <p>Accomplishment: {activity.accomplishment ? 'Yes' : 'No'}</p>
-    </div>
-  );
+    return (
+        <div>
+          <h2>Activity Detail</h2>
+          <ul className="detail-container">
+          <li>Activity Type: {activity.activityType}</li>
+          <li>Location: {activity.location}</li>
+          <li>Completion Time: {activity.completionTime}</li>
+          <li>Distance: {activity.distance}</li>
+          </ul>
+        <div className="map-container">
+            <TrackMap />
+        </div>
+        </div>
+      );    
+    
 }
 
 export default ActivityDetail;
